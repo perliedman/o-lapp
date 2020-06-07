2a5369c4f879aa64e20f9f2d0de38562d0ec9c93
Per Liedman <per@liedman.net>
2020-06-07 12:52:38 +0200

    Initial firebase setup

---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import React, { createContext } from 'react';
+import * as firebase from 'firebase/app'
+import 'firebase/analytics'
+import 'firebase/database'
+import { useReducer } from 'react'
+import { firebaseConfig } from '../config'
+
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
+
+const database = firebase.database()
+
+const initialState = {}
+
+const store = createContext(initialState)
+const { Provider } = store
+
+const StateProvider = ( {children} ) => {
+  const [state, dispatch] = useReducer(reducer)
+
+  return <Provider value={{ state, dispatch }}>{children}</Provider>
+}
+
+export { store, StateProvider }
+
+function reducer(state, action) {
+  return state
+}

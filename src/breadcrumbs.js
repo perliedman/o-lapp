aa52481df269f827624d63f4cc5d873c69bf6972
Per Liedman <per@liedman.net>
2020-07-28 12:56:02 +0200

    Add root level from breadcrumbs

--- src/breadcrumbs.js
+++ src/breadcrumbs.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Breadcrumbs({ crumbs }) {
   return <nav className="breadcrumb" aria-label="breadcrumbs">
     <ul>
-      {crumbs.map(([url, label], i) => <li key={url} className={i === crumbs.length - 1 ? 'is-active' : null}>
+      {[['/', 'o-Lapp']].concat(crumbs).map(([url, label], i) => <li key={url} className={i === crumbs.length ? 'is-active' : null}>
         <Link to={url}>{label}</Link>
       </li>)}
     </ul>

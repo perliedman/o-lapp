4ed23f96a35041efeb0684a7d027f17bf8df9a14
Per Liedman <per@liedman.net>
2020-08-30 11:06:34 +0200

    Sort by date, display date

--- src/Events.js
+++ src/Events.js
@@ -4,13 +4,25 @@ import { Link } from 'react-router-dom'
 
 export default function Events({ group, groupId }) {
   return <Query path={`/groups/${groupId}/events`} join={eventId => `/events/${eventId}`}>
-    {(events) => Object.keys(events).map((eventId) =>
-      <div key={eventId} className="box">
-        <Link to={`/event/${eventId}`}>
+    {(events) => {
+      const sortedEvents = Object.keys(events).map(id => ({
+        ...events[id],
+        id
+      })).sort((a, b) => cmp(b.date, a.date))
+
+      return sortedEvents.map((event) =>
+      <div key={event.id} className="box">
           <article>
-            <h2 className="title">{events[eventId].name}</h2>            
+            <Link to={`/event/${event.id}`}>
+              <h2 className="title">{event.name}</h2>
+            </Link>
+            {event.date}
           </article>
-        </Link>
-      </div>)}
+      </div>)
+    }}
   </Query>
+}
+
+function cmp(a, b) {
+  return a < b ? -1 : a > b ? 1 : 0
 }

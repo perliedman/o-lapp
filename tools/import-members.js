b4c3e4a59c2b110a0c0156bd8b993af008eb082b
Per Liedman <per@liedman.net>
2023-09-10 18:31:55 +0200

    Import directly from Google drive

--- tools/import-members.js
+++ tools/import-members.js
@@ -1,128 +1,165 @@
 const admin = require("firebase-admin");
-const { readFile } = require("fs");
-const stats = { nMembers: 0, nGroups: 0 };
+const { google } = require("googleapis");
+require('dotenv').config()
+
+const CREDENTIALS = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+const client = google.auth.fromJSON(CREDENTIALS);
+client.scopes = ["https://www.googleapis.com/auth/drive"];
+
+const drive = google.drive({ version: "v3", auth: client });
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
   databaseURL: "https://olapp-6c8ab.firebaseio.com",
 });
 
+const stats = { nMembers: 0, nGroups: 0 };
 const database = admin.database();
 
-readFile(process.argv[2], "utf-8", (err, data) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
+findSettingsFolderId()
+  .then((folderId) => listSpreadsheets(folderId))
+  .then((files) => files.find((f) => f.name === "TAIK Grupplista"))
+  .then(({ id }) => {
+    drive.files
+      .export({ fileId: id, mimeType: "text/csv" }, { responseType: "json" })
+      .then(({ data }) => {
+        const lines = data.split("\n").slice(1);
+        const membersRef = database.ref("/members");
 
-  const lines = data.split("\n").slice(1);
-  const membersRef = database.ref("/members");
+        membersRef.once("value", (membersSnapshot) => {
+          const members = membersSnapshot.val() || {};
 
-  membersRef.once("value", (membersSnapshot) => {
-    const members = membersSnapshot.val() || {};
-
-    const groupsRef = database.ref("/groups");
-    groupsRef.once("value", (snapshot) => {
-      const groupNameMap = {};
-      const groupMembers = {};
-      const groups = snapshot.val() || {};
-      for (let id in groups) {
-        const group = groups[id];
-        groupNameMap[group.name] = groupsRef.child(id);
-      }
-
-      lines.forEach((line) => {
-        const cols = line.split(",");
-        const isGuardian = cols[1].trim();
-        if (!isGuardian) {
-          const id = cols[4];
-          const groupName =
-            cols[0] === "Violett" || cols[0] === "Svart"
-              ? "Violett-Svart"
-              : cols[0];
-
-          let groupRef = groupNameMap[groupName];
-          if (!groupRef) {
-            groupRef = groupNameMap[groupName] = groupsRef.push();
-          }
-          if (!groupMembers[groupRef.key]) {
-            stats.nGroups++;
-            groupMembers[groupRef.key] = groups[groupRef.key]
-              ? { ...groups[groupRef.key], members: {} }
-              : { name: groupName, members: {} };
-          }
-
-          groupMembers[groupRef.key].members[id] = true;
-
-          const memberGroups = (members[id] && members[id].groups) || {};
-          memberGroups[groupRef.key] = true;
-
-          members[id] = {
-            ...members[id],
-            ...colsToMember(cols),
-            guardians: {},
-            groups: memberGroups,
-          };
-
-          membersRef.child(id).set(members[id], awaitCb());
-          stats.nMembers++;
-        } else {
-          const pattern = /Till målsman för:\s*(.*)/;
-          const childName = pattern.exec(cols[1])[1].trim();
-          const childId = Object.keys(members).find(
-            (id) => members[id].name === childName
-          );
-          if (!childId) {
-            console.error(
-              `Unable to find child with name ${childName} for guardian ${cols[2]} ${cols[3]}`
-            );
-          } else {
-            const child = members[childId];
-            let gs = child.guardians;
-            if (!gs) {
-              gs = child.guardians = {};
+          const groupsRef = database.ref("/groups");
+          groupsRef.once("value", (snapshot) => {
+            const groupNameMap = {};
+            const groupMembers = {};
+            const groups = snapshot.val() || {};
+            for (let id in groups) {
+              const group = groups[id];
+              groupNameMap[group.name] = groupsRef.child(id);
             }
-            gs[cols[4]] = colsToMember(cols);
-            membersRef.child(childId).set(child, awaitCb());
-          }
-        }
-      });
 
-      groupsRef.set(groupMembers, awaitCb());
-    });
-  });
+            lines.forEach((line) => {
+              const cols = line.split(",");
+              const isGuardian = cols[1].trim();
+              if (!isGuardian) {
+                const id = cols[4];
+                const groupName =
+                  cols[0] === "Violett" || cols[0] === "Svart"
+                    ? "Violett-Svart"
+                    : cols[0];
 
-  let waitCount = 0;
-  function awaitCb() {
-    waitCount++;
+                let groupRef = groupNameMap[groupName];
+                if (!groupRef) {
+                  groupRef = groupNameMap[groupName] = groupsRef.push();
+                }
+                if (!groupMembers[groupRef.key]) {
+                  stats.nGroups++;
+                  groupMembers[groupRef.key] = groups[groupRef.key]
+                    ? { ...groups[groupRef.key], members: {} }
+                    : { name: groupName, members: {} };
+                }
 
-    return (err) => {
-      if (err) {
+                groupMembers[groupRef.key].members[id] = true;
+
+                const memberGroups = (members[id] && members[id].groups) || {};
+                memberGroups[groupRef.key] = true;
+
+                members[id] = {
+                  ...members[id],
+                  ...colsToMember(cols),
+                  guardians: {},
+                  groups: memberGroups,
+                };
+
+                membersRef.child(id).set(members[id], awaitCb());
+                stats.nMembers++;
+              } else {
+                const pattern = /Till målsman för:\s*(.*)/;
+                const childName = pattern.exec(cols[1])[1].trim();
+                const childId = Object.keys(members).find(
+                  (id) => members[id].name === childName
+                );
+                if (!childId) {
+                  console.error(
+                    `Unable to find child with name ${childName} for guardian ${cols[2]} ${cols[3]}`
+                  );
+                } else {
+                  const child = members[childId];
+                  let gs = child.guardians;
+                  if (!gs) {
+                    gs = child.guardians = {};
+                  }
+                  gs[cols[4]] = colsToMember(cols);
+                  membersRef.child(childId).set(child, awaitCb());
+                }
+              }
+            });
+
+            groupsRef.set(groupMembers, awaitCb());
+          });
+        });
+      })
+      .catch((err) => {
         console.error(err);
         process.exit(1);
-      }
+      });
 
-      if (--waitCount <= 0) {
-        console.log(
-          `Successfully imported ${stats.nMembers} members in ${stats.nGroups} groups.`
-        );
-        process.exit(0);
-      }
-    };
-  }
+    let waitCount = 0;
+    function awaitCb() {
+      waitCount++;
 
-  function colsToMember(cols) {
-    const phone = cols[6].startsWith("46")
-      ? "+" + cols[6]
-      : cols[6].startsWith("0")
-      ? cols[6]
-      : "0" + cols[6];
+      return (err) => {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
 
-    return {
-      name: [cols[2], cols[3]].join(" "),
-      phone,
-      email: cols[7],
-      birthdate: cols[5],
-    };
-  }
-});
+        if (--waitCount <= 0) {
+          console.log(
+            `Successfully imported ${stats.nMembers} members in ${stats.nGroups} groups.`
+          );
+          process.exit(0);
+        }
+      };
+    }
+
+    function colsToMember(cols) {
+      const phone = cols[6].startsWith("46")
+        ? "+" + cols[6]
+        : cols[6].startsWith("0")
+        ? cols[6]
+        : "0" + cols[6];
+
+      return {
+        name: [cols[2], cols[3]].join(" "),
+        phone,
+        email: cols[7],
+        birthdate: cols[5],
+      };
+    }
+  });
+
+async function query(params) {
+  return await drive.files.list(params);
+}
+
+async function findSettingsFolderId() {
+  const {
+    data: {
+      files: [{ id }],
+    },
+  } = await query({
+    q: 'name="Inställningar" and mimeType="application/vnd.google-apps.folder"',
+  });
+
+  return id;
+}
+
+async function listSpreadsheets(folderId) {
+  return (
+    await query({
+      q: `"${folderId}" in parents and mimeType = "application/vnd.google-apps.spreadsheet"`,
+    })
+  ).data.files;
+}
